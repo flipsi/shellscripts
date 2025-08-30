24de00a3a5471421d4833bd79a8b601185ee33bb
Philipp Moers <soziflip@gmail.com>
2025-08-30 11:32:22 +0200

    Use my bashrc (instead of chroot)

--- linux-setup/live-env.sh
+++ linux-setup/live-env.sh
@@ -9,6 +9,8 @@
 # SELF_URL='https://github.com/flipsi/shellscripts/tree/master/linux-setup/live-env.sh'
 SELF_URL='https://raw.githubusercontent.com/flipsi/shellscripts/master/linux-setup/live-env.sh'
 
+BASHRC_URL='https://raw.githubusercontent.com/flipsi/dotfiles/master/bash/bashrc_sflip'
+
 KEYBOARD_LAYOUT='us -variant norman'
 EXTERNAL_SCREEN_ROTATION='left'
 
@@ -69,59 +71,63 @@ function get_external_screen() { # (assuming there is only one)
 }
 
 function use_external_screen_if_available() {
-	EXTERNAL_SCREEN="$(get_external_screen)"
-	LAPTOP_SCREEN="$(get_laptop_screen)"
-	if [[ -n "$EXTERNAL_SCREEN" ]] ; then
-		xrandr \
-			--output "$LAPTOP_SCREEN" --off \
-			--output "$EXTERNAL_SCREEN" --primary --auto --rotate "$EXTERNAL_SCREEN_ROTATION"
-	fi
+    EXTERNAL_SCREEN="$(get_external_screen)"
+    LAPTOP_SCREEN="$(get_laptop_screen)"
+    if [[ -n "$EXTERNAL_SCREEN" ]] ; then
+	xrandr \
+	    --output "$LAPTOP_SCREEN" --off \
+	    --output "$EXTERNAL_SCREEN" --primary --auto --rotate "$EXTERNAL_SCREEN_ROTATION"
+    fi
 }
 
 function mount_partitions() {
-	lsblk --fs
-	sudo cryptsetup open "$LUKS_PARTITION" "$LUKS_VOLUME"
-	mount "$ROOT_PARTITION" /mnt
-	mount "$BOOT_PARTITION" /mnt/boot
-	mount "$EFI_PARTITION" /mnt/boot/efi
-	mount "$HOME_PARTITION" /mnt/home
-	for d in dev proc run sys; do
-		mount -o bind "/$d" "/mnt/$d"
-	done
-	lsblk --fs
+    lsblk --fs
+    sudo cryptsetup open "$LUKS_PARTITION" "$LUKS_VOLUME"
+    mount "$ROOT_PARTITION" /mnt
+    mount "$BOOT_PARTITION" /mnt/boot
+    mount "$EFI_PARTITION" /mnt/boot/efi
+    mount "$HOME_PARTITION" /mnt/home
+    for d in dev proc run sys; do
+	mount -o bind "/$d" "/mnt/$d"
+    done
+    lsblk --fs
 }
 
 function reinstall_grub() {
-	SYSTEM_CONFIG_FILE="/etc/default/grub"
-	# GRUB_CONFIG_FILE="/boot/grub/grub.cfg"
-	GRUB_CONFIG_FILE="/boot/grub2/grub.cfg"
-	# FIXME
-	grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
-	cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
-	grub-mkconfig -o "$GRUB_CONFIG_FILE"
+    SYSTEM_CONFIG_FILE="/etc/default/grub"
+    # GRUB_CONFIG_FILE="/boot/grub/grub.cfg"
+    GRUB_CONFIG_FILE="/boot/grub2/grub.cfg"
+    # FIXME
+    grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+    cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+    grub-mkconfig -o "$GRUB_CONFIG_FILE"
 }
 
 function setup() {
-	use_external_screen_if_available
+    use_external_screen_if_available
     if ! in_X; then
 		setfont ter-220n
 	fi
 	set_keyboard_layout
 }
 
-function mount_and_chroot() {
-	mount_partitions
-	if has arch-chroot; then
-		arch-chroot /mnt
-	else
-		chroot /mnt
-	fi
+function chroot() {
+    if has arch-chroot; then
+	arch-chroot /mnt
+    else
+	env chroot /mnt
+    fi
+}
+
+function use_my_bashrc() {
+    bash --rcfile <(wget -q -O - "$BASHRC_URL")
 }
 
 function main() {
-	download_self || true
-	setup
-	mount_and_chroot
+    download_self || true
+    setup
+    mount_partitions
+    use_my_bashrc
 }
 
 set -e
